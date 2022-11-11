43a592b8ea0b014aafc8a6c5aab5039897e0cf55
ethabhijit <coder.abhijit90@gmail.com>
2022-11-11 13:56:36 +0530

    update: brand sub text

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,4 @@
-export const BRAND_NAME = "Abhijit Paul"
-export const BRAND_SUB_TEXT = "Indian full stack developer for Haskell, Plutus & Solidity"
+export const BRAND_NAME = 'Abhijit Paul'
+export const BRAND_SUB_TEXT =
+  'Full stack developer for Cardano & Ethereum Dapps'
 export const NavLinks = ['Education', 'Projects', 'About']
