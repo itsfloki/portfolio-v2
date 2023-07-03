a073bb8707517de9f3823308e5927472b0bcc4b9
itsfloki <coder.abhijit90@gmail.com>
2023-07-03 17:59:18 +0530

    add: hover caption project

--- components/ProjectContainer/ProjectCard.tsx
+++ components/ProjectContainer/ProjectCard.tsx
@@ -1,16 +1,16 @@
 import type { NextComponentType, NextPageContext } from 'next'
 import {
   Box,
-  Badge,
   Button,
-  Center,
-  Flex,
   Heading,
   Image,
-  Stack,
   Text,
+  Stack,
+  Badge,
   useColorModeValue,
 } from '@chakra-ui/react'
+import { FaGithub } from 'react-icons/fa'
+import { BsArrowUpRight } from 'react-icons/bs'
 
 interface ProjectCardProps {
   config: {
@@ -29,7 +29,7 @@ const ProjectCard: NextComponentType<NextPageContext, {}, ProjectCardProps> = ({
   const { title, description, github_link, live_link, image, tags } = config
 
   return (
-    <>
+    <Box pos="relative" role="group" cursor={'pointer'}>
       <Image
         objectFit="cover"
         w="full"
@@ -40,7 +40,57 @@ const ProjectCard: NextComponentType<NextPageContext, {}, ProjectCardProps> = ({
         border="3px solid"
         borderColor={useColorModeValue('gray.100', 'gray.900')}
       />
-    </>
+
+      <Box
+        pos="absolute"
+        borderRadius="md"
+        w="full"
+        h="full"
+        bg={useColorModeValue('gray.50', 'gray.700')}
+        bottom={0}
+        p={5}
+        opacity={0}
+        transition="all 300ms ease-in-out"
+        _groupHover={{ opacity: 1 }}
+      >
+        <Heading size="md" color={useColorModeValue('gray.900', 'gray.200')}>
+          {title}
+        </Heading>
+
+        <Text>{description}</Text>
+
+        <Stack direction="row" mt={5}>
+          {tags.map((tag, idx) => (
+            <Badge colorScheme="default" key={tag + '-' + idx}>
+              {tag}
+            </Badge>
+          ))}
+        </Stack>
+
+        <Stack direction="row" mt={5}>
+          <Button
+            rounded={'full'}
+            size="sm"
+            leftIcon={<FaGithub />}
+            as={'a'}
+            href={github_link}
+            target={'_blank'}
+          >
+            Github
+          </Button>
+          <Button
+            rounded={'full'}
+            size="sm"
+            leftIcon={<BsArrowUpRight />}
+            as={'a'}
+            href={live_link}
+            target={'_blank'}
+          >
+            Live
+          </Button>
+        </Stack>
+      </Box>
+    </Box>
   )
 }
 
