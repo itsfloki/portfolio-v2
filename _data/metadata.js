24b04447f111919263752a1b208e2f0a6078a3a1
itsfloki <coder.abhijit90@gmail.com>
2023-10-17 12:38:13 +0530

    add: blog feature; update: style

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "Abhijit's Blog",
+	url: "https://example.com/",
+	language: "en",
+	description: "I am writing about my experiences as a naval navel-gazer.",
+	author: {
+		name: "Abhijit Paul",
+		email: "coder.abhijit90@.com",
+		url: "https://devabhijit.in/"
+	}
+}
